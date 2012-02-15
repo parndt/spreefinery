c3d70a7329239f297f6b9f2d2a76997703a2154e
Philip Arndt <parndt@gmail.com>
2012-02-15 15:03:19 +1300

    kludge will_paginate

---
+++ config/initializers/spreefinery.rb
@@ -0,0 +1,10 @@
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        alias_method :per, :per_page
+        alias_method :num_pages, :total_pages
+      end
+    end
+  end
+end
